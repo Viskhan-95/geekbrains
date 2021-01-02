5b84f54f6bae43a4d24a7e864b34430858f0ee50
viskhan <rustam1987@mail.ru>
2021-01-02 14:53:09 +0300

    sent DZ2

---
+++ DZ2/DZ2.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+                Console.WriteLine("На ноль нельзя делить");
+            else if (num % 2 == 0)
+                Console.WriteLine("Введенное число является четным");
+            else
+                Console.WriteLine("Введенное число является нечетным");
+
+            Console.ReadKey();
+        }
+    }
+}
