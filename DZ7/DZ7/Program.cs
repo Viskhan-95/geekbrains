2f5ffadb104cac47a508cebf599cd03a45eb61bc
viskhan <rustam1987@mail.ru>
2021-01-04 21:49:25 +0300

    send DZ7

---
+++ DZ7/DZ7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Вывести ваши данные на экран?");
+            string answerUser = Console.ReadLine();
+
+            if (answerUser.ToLower() == "да")
+            {
+                Console.WriteLine($"Вас зовут {lastName}. Вам {age} лет.");
+            }
+            else
+                Console.WriteLine("Спасибо!");
+        }
+    }
+}
