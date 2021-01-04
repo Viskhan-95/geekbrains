4bd3ba159ff5804b542c6d1955895f465e21ac8c
viskhan <rustam1987@mail.ru>
2021-01-04 11:07:41 +0300

    sent DZ5

---
+++ DZ5/DZ5.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DZ5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа от 0 до 255: ");
+
+            byte[] binary = System.Text.Encoding.UTF8.GetBytes(Console.ReadLine());
+
+            string fileName = "binaryFile.bin";
+            //string fileName = "binaryFile.txt";
+
+            File.WriteAllBytes(fileName, binary);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ5/DZ5.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace DZ5._5
+{
+    class Program
+    {
+        static void Main(string[] args) //Хотелось бы это задание решить совместно, для меня трудная задача на данный момент
+        {
+            int count = 1;
+            if (File.Exists("tasks.json"))
+            {
+                string toDoStr = File.ReadAllText("tasks.json");
+                var toDo1 = JsonSerializer.Deserialize<ToDo>(toDoStr);
+                Console.Write($"{toDo1.Number} ");
+                Console.WriteLine(toDo1.Title);
+                count = toDo1.Number + 1;
+            }
+
+            ToDo toDo = new ToDo();
+            Console.Write("Задача: ");
+            toDo.Title = Console.ReadLine();
+            toDo.Number = count;
+            toDo.IsDone = false;
+
+            string json = JsonSerializer.Serialize(toDo);
+            File.AppendAllText("tasks.json", json);
+
+            Console.ReadKey();
+        }
+    }
+}
